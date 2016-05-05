96395e7bd93d810ef28246413ad511021f007f73
Washcloth <cjwalsh@live.com.au>
2016-05-05 15:11:26 +1000

    added painting
    added painting to intro scene, me skuzi

--- Assets/paintingActive.cs
+++ Assets/paintingActive.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class paintingActive : MonoBehaviour {
 
+	float counter;
+ 	GameObject painting;
+
 	// Use this for initialization
 	void Start () {
-	
+
+		painting = GameObject.Find ("ArtFrame");
+		counter = 15f;
+		painting.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+		counter -= Time.deltaTime;
+		if (counter <= 0)
+		{
+			painting.SetActive(true);
+		}
 	}
 }
