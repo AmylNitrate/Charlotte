13870ae811bd547eec8bee18cac94af43542204b
Washcloth <cjwalsh@live.com.au>
2016-05-05 12:56:56 +1000

    added in scene
    added transition scene

---
+++ Assets/_scripts/levelTimer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class levelTimer : MonoBehaviour {
+
+	float counter;
+	// Use this for initialization
+	void Start () {
+		
+		counter = 45f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		counter -= Time.deltaTime;
+		if (counter <= 0)
+		{
+			SceneManager.LoadScene("SceneOne");
+		}
+	}
+}
